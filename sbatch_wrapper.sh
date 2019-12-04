3f0d0ef8f1a5f24da5ceb1283e70774e46219e4d
P-O Quirion <pioliqui@gmail.com>
2019-12-04 15:31:22 -0500

    put parallel_wrapper.sh in heredoc

--- sbatch_wrapper.sh
+++ sbatch_wrapper.sh
@@ -2,28 +2,105 @@
 usage () {
 
 cat  << EOF
-  $0  <list of .bed files> <vcf output directory>
+  $0  <dict> <N hosts> <N CHUNKS> <output directory>
+  <dict>:
+    File In in the form of Homo_sapiens.GRCh38.dict found in
+      /cvmfs/ref.mugqic/genomes/species/Homo_sapiens.GRCh38/genome
+  <N hosts>
+    How many Host per chromosome, all the cores are taken
+  <N CHUNKS>
+    Number of chunks per chromosome to split your merge into. Should be at
+      least 200 * <N host> so it make sense to use that code.
+  <output directory>
+    Where the log and the job is keep
+  -r
+    Rerun the job found in <output directory>
 
-  if you bed files are all in a directory structure, you can put them in a file with the following command:
-	  $ lfs find  \$(realpath ./path_to/bed_files/) -type f -name "*.bed" -ls > my_list_of_bed.txt  
+
 EOF
 }
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 4 ]; then
   usage
   exit 1
 fi
 
-export BED_FILES=$1
-export VCF_OUTPUT_DIR=$2
+
+
+INPUT_DICT=$1
+CHUNKS=$3
+N_HOSTS=$2
+VCF_OUTPUT_DIR=$4
+
+export INTERVAL_DIR=
+
+# create one bed file per chr
+
+make_inputs (){
+
+    INPUT_DICT=$1
+    CHUNKS=$2
+    VCF_OUTPUT_DIR=$3
+
+    INTERVAL_DIR=$VCF_OUTPUT_DIR/intervals
+    mkdir -p $INTERVAL_DIR
+    range_per_chr=($( grep "chr[1-9XY][0-9]*\s"  $INPUT_DICT | sed 's/.*SN:chr\([0-9XY]\+\)\sLN:\([0-9]\+\).*/\1 \2/' ))
+
+    for chr in {0..23}; do
+       val=$((2*$chr))
+       chr_range=(${range_per_chr[@]:$val:2})
+
+       INTERVAL_PATH=$INTERVAL_DIR/chr_${chr_range[0]}
+
+       range=$((${chr_range[1]}/$((CHUNKS-1))))
+       previous=1
+       rm $INTERVAL_PATH 2> /dev/null
+       for ((i="$range" ; i<=${chr_range[1]} ; i+=$range));do
+
+           echo chr${chr_range[0]}:$previous-$i >> $INTERVAL_PATH
+           previous=$i
+       done
+           echo chr${chr_range[0]}:$previous-${chr_range[1]} >> $INTERVAL_PATH
+
+    done
+
+}
 
 
 mkdir -p ${VCF_OUTPUT_DIR}
 
+make_inputs $INPUT_DICT $CHUNKS $VCF_OUTPUT_DIR
+
 rdn_str=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 master_name=vcf_merge_master_${rdn_str}
 
-echo submit master job
-job_id=$(sbatch -A $RAP_ID  --time 1-00:00:00 --mem-per-cpu=4775  --ntasks-per-node=40 --nodes=5 \
-       	--job-name=$master_name  --output=${master_name}.slurm-%j.out  parallel_wrapper.sh | awk '{print $NF}' )
-echo $job_id submited
+
+
+for file in $INTERVAL_DIR/chr* ; do
+
+  sbatch_file=$(mktemp /tmp/gnu_parallel_XXXXXX.sh)
+
+#Will test read to var maybe
+#read -r -d '' VAR <<'EOF'
+
+  cat << EOF >$sbatch_file
+#!/usr/bin/env bash
+
+scontrol show hostname \${SLURM_JOB_NODELIST} > $VCF_OUTPUT_DIR/node_list_\${SLURM_JOB_ID}
+
+
+parallel --joblog $VCF_OUTPUT_DIR/job.log  --resume-failed  --jobs \${SLURM_CPUS_ON_NODE} --sshloginfile \
+ ${VCF_OUTPUT_DIR}/node_list_\${SLURM_JOB_ID}  --workdir $PWD --env _   -a $file ./gatk_wrapper.sh
+EOF
+
+
+# echo submit master job
+# job_id=$(sbatch -A $RAP_ID  --time 1-00:00:00 --mem-per-cpu=4775  --ntasks-per-node=40 --nodes=$N_HOSTS \
+#        	--job-name=$master_name  --output=${master_name}.slurm-%j.out  parallel_wrapper.sh | awk '{print $NF}' )
+# echo $job_id submited
+
+  sbatch -A $RAP_ID  --time 1-00:00:00 --mem-per-cpu=4775  --ntasks-per-node=40 --nodes=$N_HOSTS \
+    --job-name=$master_name  --output=${master_name}.slurm-%j.out  $sbatch_file
+
+
+done
